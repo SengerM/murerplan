82a2a9097e64a02a9dbe6da4c603bb01782299ff
Matias Senger <m.senger@hotmail.com>
2024-05-13 22:11:21 +0200

    Some improvements

--- build.py
+++ build.py
@@ -2,6 +2,10 @@ import dominate # https://github.com/Knio/dominate
 from pathlib import Path
 import json
 
+def image_with_link_to_itself(src, **kwargs):
+	with dominate.tags.a(href=src):
+		dominate.tags.img(src=src, **kwargs),
+
 def create_thing_page(path_to_thing_folder:Path):
 	thing_id = path_to_thing_folder.name
 
@@ -23,7 +27,7 @@ def create_thing_page(path_to_thing_folder:Path):
 			dominate.tags.li(thing_data['address'])
 
 		# Links to maps:
-		with dominate.tags.div(style='display: flex; flex-direction: row; flex-wrap: wrap; gap: 10px;'):
+		with dominate.tags.div(style='display: flex; flex-direction: row; flex-wrap: wrap; gap: 10px; margin: 22px;'):
 			lat = thing_data['coordinates'][0]
 			lon = thing_data['coordinates'][1]
 			with dominate.tags.a(href=f'https://www.google.com/maps/place/{lat},{lon}/@{lat},{lon-.003},222a,35y,90h,50t/data=!3m1!1e3!4m4!3m3!8m2!3d{lat}!4d{lon}?entry=ttu'):
@@ -33,10 +37,16 @@ def create_thing_page(path_to_thing_folder:Path):
 					title = 'Open in Google Maps',
 				)
 
+		path_to_thing_in_murerplan = path_to_thing_folder/'in_murerplan.jpg'
+		if path_to_thing_in_murerplan.is_file():
+			image_with_link_to_itself(
+				src = path_to_thing_in_murerplan.relative_to(path_to_thing_folder),
+				style = 'max-width: 99%; height: 222px; object-fit: cover; border-radius: 5px;',
+			)
+
 		with dominate.tags.div(style='display: flex; flex-direction: row; flex-wrap: wrap; gap: 10px; margin-top: 22px;'):
 			for path_to_pic in (path_to_thing_folder/'pics').iterdir():
-				with dominate.tags.a(href=path_to_pic.relative_to(path_to_thing_folder)):
-					dominate.tags.img(src=path_to_pic.relative_to(path_to_thing_folder), style='max-width: 99%; height: 222px; object-fit: cover; border-radius: 5px;')
+				image_with_link_to_itself(src=path_to_pic.relative_to(path_to_thing_folder), style='max-width: 99%; height: 222px; object-fit: cover; border-radius: 5px;')
 
 	with open(path_to_thing_folder/'index.html', 'w') as ofile:
 		print(doc, file=ofile)
