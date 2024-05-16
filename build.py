0f0d78620b06fde3d2140324e6b6df6394b2821b
Matias Senger <m.senger@hotmail.com>
2024-05-16 11:43:32 +0200

    Improved things pages to be more responsive

--- build.py
+++ build.py
@@ -7,6 +7,8 @@ def image_with_link_to_itself(src, **kwargs):
 		dominate.tags.img(src=src, **kwargs),
 
 def create_thing_page(path_to_thing_folder:Path):
+	PICTURES_BORDER = 'border-radius: 5px; border-style: solid; border-color: #4e4f4e;'
+
 	thing_id = path_to_thing_folder.name
 
 	with open(path_to_thing_folder/'data.json', 'r') as ifile:
@@ -22,28 +24,32 @@ def create_thing_page(path_to_thing_folder:Path):
 		dominate.tags.meta(name="viewport", content="width=device-width, initial-scale=1") # This fixes the problem of small font (some texts and also the math) in mobile devices, see https://stackoverflow.com/a/35564095/8849755
 
 	with doc:
-		dominate.tags.h1(thing_data['name'])
+		with dominate.tags.div(style='display:flex; gap: 10px; flex-wrap: wrap; flex-direction: row-reverse; justify-content: flex-end;'):
+			with dominate.tags.div():
+				dominate.tags.h1(thing_data['name'])
 
-		# Links to maps:
-		lat = thing_data['coordinates'][0]
-		lon = thing_data['coordinates'][1]
-		with dominate.tags.div(style='margin: 22px;'):
-			dominate.tags.a(thing_data['address'], href=f'https://www.google.com/maps/place/{lat},{lon}/@{lat},{lon-.0015},111a,35y,90h,50t/data=!3m1!1e3!4m4!3m3!8m2!3d{lat}!4d{lon}?entry=ttu')
+				lat = thing_data['coordinates'][0]
+				lon = thing_data['coordinates'][1]
+				dominate.tags.a(thing_data['address'], href=f'https://www.google.com/maps/place/{lat},{lon}/@{lat},{lon-.0015},111a,35y,90h,50t/data=!3m1!1e3!4m4!3m3!8m2!3d{lat}!4d{lon}?entry=ttu')
 
-		for extension in {'jpg','png','svg'}:
-			path_to_thing_in_murerplan = path_to_thing_folder/f'in_murerplan.{extension}'
-			if path_to_thing_in_murerplan.is_file():
-				image_with_link_to_itself(
-					src = path_to_thing_in_murerplan.relative_to(path_to_thing_folder),
-					style = 'max-width: 99%; max-height: 45vh; height: 333px; object-fit: cover; border-radius: 5px;',
-				)
-				break
+			with dominate.tags.div():
+				for extension in {'jpg','png','svg'}:
+					path_to_thing_in_murerplan = path_to_thing_folder/f'in_murerplan.{extension}'
+					if path_to_thing_in_murerplan.is_file():
+						dominate.tags.img(
+							src = path_to_thing_in_murerplan.relative_to(path_to_thing_folder),
+							style = 'height: 45vh;' + PICTURES_BORDER,
+						)
+						break
 
 		path_to_pics = path_to_thing_folder/'pics'
 		if path_to_pics.is_dir():
-			with dominate.tags.div(style='display: flex; flex-direction: row; flex-wrap: wrap; gap: 10px; margin-top: 22px;'):
+			with dominate.tags.div(style='display: flex; flex-direction: row; flex-wrap: no-wrap; gap: 10px; margin-top: 10px; overflow: auto;'):
 				for path_to_pic in path_to_pics.iterdir():
-					image_with_link_to_itself(src=path_to_pic.relative_to(path_to_thing_folder), style='max-width: 99%; max-height: 45vh; height: 333px; object-fit: cover; border-radius: 5px;')
+					dominate.tags.img(
+						src = path_to_pic.relative_to(path_to_thing_folder),
+						style = 'max-height: 45vh; height: 333px;' + PICTURES_BORDER,
+					)
 
 	with open(path_to_thing_folder/'index.html', 'w') as ofile:
 		print(doc, file=ofile)
